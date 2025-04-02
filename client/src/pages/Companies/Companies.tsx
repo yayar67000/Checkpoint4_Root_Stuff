6b01f0ea2a88913936c6608fef6dfd6413351213
JULIAN HAZARD <hazardjulian74@gmail.com>
2025-04-02 16:04:55 +0200

    Page Vans

--- client/src/pages/Companies/Companies.tsx
+++ client/src/pages/Companies/Companies.tsx
@@ -1,9 +1,21 @@
+import { Link, useLoaderData } from "react-router-dom";
+import CompanyCard from "../../components/CompanyCard/CompanyCard";
 import "./Companies.css";
 
 export default function Companies() {
+  const allCompanies = useLoaderData() as CompaniesDetailData[];
   return (
-    <main>
-      <h1>Companies</h1>
+    <main className="all_companies_page">
+      <h1>Les entreprises</h1>
+      <div>
+        {allCompanies.length > 0
+          ? allCompanies.map((company) => (
+              <Link key={company.id} to={`/companyDetails/${company.id}`}>
+                <CompanyCard key={company.id} company={company} />
+              </Link>
+            ))
+          : null}
+      </div>
     </main>
   );
 }
--- client/src/pages/Vans/Vans.tsx
+++ client/src/pages/Vans/Vans.tsx
@@ -1,9 +1,21 @@
+import { Link, useLoaderData } from "react-router-dom";
+import VanCard from "../../components/VanCard/VanCard";
 import "./Vans.css";
 
 export default function Vans() {
+  const allVans = useLoaderData() as VansData[];
   return (
-    <main>
+    <main className="all_vans_page">
       <h1>Vans</h1>
+      <div>
+        {allVans.length > 0
+          ? allVans.map((van) => (
+              <Link key={van.id} to={`/vansDetails/${van.id}`}>
+                <VanCard key={van.id} van={van} />
+              </Link>
+            ))
+          : null}
+      </div>
     </main>
   );
 }
