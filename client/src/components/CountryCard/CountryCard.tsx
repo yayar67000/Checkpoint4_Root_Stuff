6158be904ed82ea7fb9fe38b44722c3e09c40424
JULIAN HAZARD <hazardjulian74@gmail.com>
2025-06-25 10:30:06 +0200

    Allgement_code_css_regroupement_app.css

--- client/src/components/CountryCard/CountryCard.tsx
+++ client/src/components/CountryCard/CountryCard.tsx
@@ -1,9 +1,7 @@
-import "./CountryCard.css";
-
 export default function CountryCard({ country }: CountriesDataProps) {
   return (
     <>
-      <div className="country_card">
+      <div className="card_item">
         <img src={country.picture} alt={`country_picture${country.name}`} />
         <h2>{country.name}</h2>
       </div>
