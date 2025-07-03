c8cd6b7141277b6fd38e9a8176ec25484e9a0ea7
JULIAN HAZARD <hazardjulian74@gmail.com>
2025-07-03 17:15:11 +0200

    debug

--- server/src/app.ts
+++ server/src/app.ts
@@ -23,10 +23,6 @@ app.use(cookieParser());
 
 import cors from "cors";
 
-if (process.env.CLIENT_URL != null) {
-  app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }));
-}
-
 // If you need to allow extra origins, you can add something like this:
 const allowedOrigins = [
   "https://rootstuff.netlify.app",
