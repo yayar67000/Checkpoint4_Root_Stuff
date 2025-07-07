5fc5e6511b00e5c3fa5624543d70507df7e5d933
JULIAN HAZARD <hazardjulian74@gmail.com>
2025-07-07 16:14:01 +0200

    correction-deploiement-front

--- server/src/app.ts
+++ server/src/app.ts
@@ -24,11 +24,9 @@ app.use(cookieParser());
 import cors from "cors";
 
 // If you need to allow extra origins, you can add something like this:
-const allowedOrigins = [
-  process.env.HOST_URL,
-  process.env.CLIENT_URL,
-  process.env.DEV_URL,
-].filter((origin): origin is string => typeof origin === "string");
+const allowedOrigins = [process.env.DEV_URL, process.env.CLIENT_URL].filter(
+  (origin): origin is string => typeof origin === "string",
+);
 
 app.use(
   cors({
